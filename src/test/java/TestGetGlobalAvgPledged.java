9a8aa2ac2086faf098bbabf8ef71fe861b368f09
nico <nicolas.chausseau@gmail.com>
2018-02-07 17:03:45 -0500

    ADD: a simple dummy unit test for the avg pledged function

---
+++ src/test/java/TestGetGlobalAvgPledged.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import csv.CsvReader;
+import stats.KickstarterStats;
+
+public class TestGetGlobalAvgPledged {
+
+	@Test
+	public void testNotNull() {
+		
+		CsvReader csvReader = new CsvReader();
+		KickstarterStats classUnderTest = new KickstarterStats(csvReader);
+        assertNotNull("making sure that a non-null avg pledged is returned", classUnderTest.getAvgOfPledges());
+		
+	}
+	
+	
+	
+
+}
+
+
